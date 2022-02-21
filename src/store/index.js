5fa2167b52d4ced1124424696620221484f6362c
张坤 <2553150278@qq.com>
2022-02-21 17:45:48 +0800

    弃用人人开源并改用自建Vue3,初始化Vue项目结构

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import cloneDeep from 'lodash/cloneDeep'
-import common from './modules/common'
-import user from './modules/user'
+import { createStore } from 'vuex'
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  modules: {
-    common,
-    user
+export default createStore({
+  state: {
   },
   mutations: {
-    // 重置vuex本地储存状态
-    resetStore (state) {
-      Object.keys(state).forEach((key) => {
-        state[key] = cloneDeep(window.SITE_CONFIG['storeState'][key])
-      })
-    }
   },
-  strict: process.env.NODE_ENV !== 'production'
+  actions: {
+  },
+  modules: {
+  }
 })
